6d28a6ccfec2c9ee424410bd077a02d68951b64a
lkyahpu <44027069+lkyahpu@users.noreply.github.com>
2024-06-07 09:27:39 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -56,14 +56,14 @@ def test():
 
     with torch.no_grad():
         for idx_iter, (img, size, img_dir) in tqdm(enumerate(test_loader)):
-            if size[0] > 640 or size[1] > 640:
+            if size[0] > 512 or size[1] > 512:
                 down = nn.Upsample(scale_factor=0.25, mode='bilinear', align_corners=True)
                 img = down(img)
             # img = transform1(img)
 
             img = Variable(img).cuda()
             pred = net.forward(img)
-            if size[0] > 640 or size[1] > 640:
+            if size[0] > 512 or size[1] > 512:
                 up = nn.Upsample(scale_factor=4, mode='bilinear', align_corners=True)
                 pred = up(pred)
             # transform2 = transforms.Compose([transforms.Resize((size[0], size[1]), antialias=True)])
